5f5847c879f022eb9bd3cd5c0ceed87c9dcf6c8d
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-11-12 10:39:52 -0800

    pretty

--- index.js
+++ index.js
@@ -3,50 +3,33 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname, '../frontend/src/Images')));
-
-
-
-
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "../frontend/src/Images"))
+);
 
 const router = require("./routes/productroute");
 const route = require("./routes/userRoute");
-const orderRouter  =require("./routes/orderRoute")
+const orderRouter = require("./routes/orderRoute");
 
-
-
-const stripe = require("stripe") (process.env.STRIPE_SECRET_KEY)
-
-
-
-
-
-
-
-
-
-
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("the db is running successfully"))
   .catch((err) => console.log(err));
 
-
-
-
-  app.use("/", route);
-  app.use("/", router);
-app.use("/", orderRouter)
+app.use("/", route);
+app.use("/", router);
+app.use("/", orderRouter);
 app.post("/create-checkout-sessions", async (req, res) => {
   try {
     const { products } = req.body;
@@ -55,11 +38,11 @@ app.post("/create-checkout-sessions", async (req, res) => {
       price_data: {
         currency: "usd",
         product_data: {
-          name: item.name ,
-                },
-        unit_amount: item.price*100 ,
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
       },
-      quantity: item.quantity 
+      quantity: item.quantity,
     }));
 
     const session = await stripe.checkout.sessions.create({
@@ -67,7 +50,7 @@ app.post("/create-checkout-sessions", async (req, res) => {
       line_items: lineItems,
       mode: "payment",
       success_url: "http://localhost:5173/",
-      cancel_url: "http://localhost:3000/cancel"
+      cancel_url: "http://localhost:3000/cancel",
     });
 
     res.json({ id: session.id });
@@ -75,8 +58,6 @@ app.post("/create-checkout-sessions", async (req, res) => {
     res.status(500).send(`Error creating checkout session: ${error.message}`);
   }
 });
-
-
 
 app.listen(port, () => {
   console.log("server is running on port", port);
